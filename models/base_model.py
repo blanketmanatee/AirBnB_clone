910cfa1e54e62f40b7d92e442fd95e45e97c3744
ArtisanGray <1697@holbertonschool.com>
2021-02-12 20:59:20 +0000

    fixed class name error and datetime

--- models/base_model.py
+++ models/base_model.py
@@ -5,34 +5,34 @@ import uuid
 
 
 class BaseModel():
-    """ 
+    """
     defines all common attributes/methods for other classes
     Public instance attributes
     id - string assigned with uuid when an instance is created
-    created_at - datetime assign with current datetime when an instance is created
-    updated_at - datetime assign with current datetime & updated when object is changed
+    created_at - current datetime when an instance is created
+    updated_at - current datetime updated when object is changed
     __str__ - should print [<class name>] (<self.id>) <self.__dict__>
 
     Public instance methods
     save(self) - updates updated_at with current datetime
-    to_dict(self) - returns a dictionary containing all keys/values of __dict__ of the instance
+    to_dict(self) - returns a dictionary representation of the instance
     by using self.__dict__ only instance attributes are returned
     a key __class__ must be added to the dictionary w class name of object
     created_at and updated_at must be converted to string object in ISO format
     """
     def __init__(self, id='', created_at='', updated_at=''):
         """ initialization """
-        self.id= str(uuid.uuid4())
-        self.created_at= str(datetime.now)
-        self.updated_at= str(datetime.now)
-        
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
     def save(self):
-        self.updated_at= str(datetime.now)
-        
+        self.updated_at = datetime.now()
+
     def to_dict(self):
         new_dict = self.__dict__
-        new_dict['__class__'] = type(self).__name__
+        new_dict['__class__'] = self.__class__.__name__
         return new_dict
 
     def __str__(self):
-        return f'{self.id} {self.__dict__}'
+        return f"[{self.__class__.__name__}] " + f'({self.id}) {self.__dict__}'
