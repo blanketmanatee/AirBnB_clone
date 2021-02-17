95c2b1a9f77c9da49d559d41bf904de1d9292fef
blanketmanatee <ashleyl.welch@gmail.com>
2021-02-16 23:05:55 -0600

    added more tests for basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -18,4 +18,16 @@ class TestBaseModel(unittest.TestCase):
         """ tests instance of class """
         base = BaseModel()
         self.assertTrue(isinstance(base, BaseModel))
-        
+    
+    def test_to_dict(self):
+        """ tests dictionary """
+        base = BaseModel()
+        return_dict = base.to_dict()
+        self.assertTrue(isinstance(return_dict, dict))
+    
+    def test_str(self):
+        """ tests str """
+        base = BaseModel()
+        BaseStr = base.__str__()
+        self.assertTrue(isinstance(BaseStr, str))
+    
